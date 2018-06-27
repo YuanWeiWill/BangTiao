f468d49050a574fbd998c2db27d3ad5bf59538f1
gy <gangyun@gydeimac.local>
2018-06-27 14:32:29 +0800

    Initial Commit

---
+++ bangtiao/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  bangtiao
+//
+//  Created by gy on 2018/6/27.
+//  Copyright © 2018年 gy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
